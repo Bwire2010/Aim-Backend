fcd902aa5a2d2dc8dc179b225e4863d663b13b8e
Patrobas Bwire <patrobasbwire@gmail.com>
2023-01-21 12:21:48 +0300

    created resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :requests
+  resources :employees
+  resources :categories
+  resources :assets
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
