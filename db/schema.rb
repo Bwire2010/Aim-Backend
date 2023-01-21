e73bbc8f626c670055d1f19360e9e4ff4cb027bc
Patrobas Bwire <patrobasbwire@gmail.com>
2023-01-21 13:22:17 +0300

    seeding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_21_092101) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_21_101836) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_21_092101) do
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
   create_table "employees", force: :cascade do |t|
