9217c85be634ced66df904c8e971ed3bbe408bb5
Patrobas Bwire <patrobasbwire@gmail.com>
2023-01-21 21:37:50 +0300

    changes in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       # byebug
       if user&.authenticate(params[:password])
         session[:user_id] = user.id
-        render json: Asset.all
+        render json: user, status: :created
       else
         render json: { error: { login: 'Invalid Username or password' } }, status: :unauthorized
       end
