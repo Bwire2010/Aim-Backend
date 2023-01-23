bb4a9e16d1cd39552a76a7f7a98f70850e966767
Patrobas Bwire <patrobasbwire@gmail.com>
2023-01-23 11:15:45 +0300

    auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,16 @@ class ApplicationController < ActionController::API
 
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   
-    #  before_action :authorize
+     skip_before_action :authorize
     
   
-    # private
-    # # if the user is not logged in when they make the request, 
-    # def authorize
-    #   @current_user = User.find_by(id: session[:user_id])
+    private
+    # if the user is not logged in when they make the request, 
+    def authorize
+      @current_user = User.find_by(id: session[:user_id])
   
-    #   render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
-    # end
+      render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+    end
   
     def render_unprocessable_entity_response(exception)
       render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
