10aa3faf020bcc2691d2d15eb05bbb76690f5804
patrobas <patrobasbwire@gmail.com>
2024-01-19 16:19:06 +0300

    render build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 # exit on error
-# set -o errexit
-
-# bundle install
-# bundle exec rake db:migrate
-# bundle exec rake db:seed
 set -o errexit
 
 bundle install
-./bin/rails assets:precompile
-./bin/rails assets:clean
+bundle exec rake db:migrate
+bundle exec rake db:seed
